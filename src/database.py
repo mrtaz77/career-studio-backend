371edad743333db38a8f0c8c5c50e842c2fe7322
mrtaz77 <mrtazwar23@gmail.com>
2025-05-08 13:42:19 +0600

    refactor: update database

--- src/database.py
+++ src/database.py
@@ -1,9 +1,9 @@
 from contextlib import asynccontextmanager
+from logging import getLogger
 
 from fastapi import FastAPI
 
 from src.generated.prisma import Prisma
-from logging import getLogger
 
 logger = getLogger(__name__)
 db = Prisma()
@@ -14,6 +14,7 @@ async def lifespan(app: FastAPI):
     logger.info("Connecting to the database...")
     await db.connect()
     logger.info("Database connection established.")
+
     try:
         yield
     finally:
